91378f776bd4e8985d6928e5d7f6d3b7c13a2eb1
YOHIGH <45271275+YOHIGH@users.noreply.github.com>
2020-09-12 23:39:08 +0530

    Update RockPaperScissor.py

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -3,49 +3,39 @@ import os
 import time
 
 
+def converted(number):
+    if number == 1:
+        return 'rock'
+    elif number == 2:
+        return 'paper'
+    elif number == 3:
+        return 'scissor'
+
+
+Outcomes = {
+    'rock': {'rock': 'draw', 'paper': 'lost', 'scissor': 'won'},
+    'paper': {'rock': 'won', 'paper': 'draw', 'scissor': 'lost'},
+    'scissor': {'rock': 'lost', 'paper': 'won', 'scissor': 'draw'}
+}
+
+Mylist = ['rock', 'paper', 'scissor']
 while True:
     os.system('cls||clear')
+    Bot_choice = random.choice(Mylist)
+    print("Let's play ROCK PAPER SCISSOR\n\nChoose your option\n1)ROCK\n2)PAPER\n3)SCISSOR\n")
+
     try:
-        mylist = ['ROCK', 'PAPER', 'SCISSOR']
-        Bot = random.choice(mylist)
-        print("Let's play ROCK PAPER SCISSOR\n\nChoose your option\n1)ROCK\n2)PAPER\n3)SCISSOR\n")
-        inp = int(input("Enter your choice=>"))
-
-        if inp == 1:
-            if 'ROCK' == Bot:
-                print("\nTIE.Try again")
-            else:
-                if Bot == 'PAPER':
-                    print("\nAh! Your Rock lost against Paper")
-                else:
-                    print("\nOh! You won against Scissor")
-
-        if inp == 2:
-            if 'PAPER' == Bot:
-                print("\nTIE.Try again")
-            else:
-                if Bot == 'ROCK':
-                    print("\nOh! Your PAPER won against Rock")
-                else:
-                    print("\nAh! You lost against Scissor")
-
-        if inp == 3:
-            if 'SCISSOR' == Bot:
-                print("\nTIE.Try again")
-            else:
-                if Bot == 'PAPER':
-                    print("\nOh! Your Scissor won against Paper\n")
-                else:
-                    print("\nAh! You lost against Rock\n")
-        if inp < 1 or inp > 3:
-            print('\n"invalid choice"\n')
+        user_choice = int(input("Enter your choice {1,2,3}=>"))
+        Converted_userchoice = converted(user_choice)
+        print('\n======Result======\n')
+        print('==>', Outcomes[Converted_userchoice][Bot_choice], '<==')
+        print('\nYour Choice:', Converted_userchoice)
+        print('\nComputer Choice:', Bot_choice)
         print("========================================")
-        print("Want to play more!? Type 'play'")
-        Taken_inp = input('=>')
-        if Taken_inp == 'play':
-            pass
-        else:
-            break
+        print('plz wait for 5 sec after it will automatically restart')
+        time.sleep(5)
     except:
-        print('Invalid argument')
-        time.sleep(1)
+        print('Invalid arguments')
+        print("please use Numeric values between 1 to 3")
+        print('\nplz wait for 3 sec after it will automatically restart')
+        time.sleep(3)
